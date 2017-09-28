76e00c0f210e95d776677c59f0a8b408d3a9f46f
Guilherme Hartmann Sales <guilhermehsales@mbpdeguilherme.home>
2017-09-28 13:17:11 -0300

    Initial Commit

---
+++ 01/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  01
+//
+//  Created by Guilherme Hartmann Sales on 28/09/17.
+//  Copyright © 2017 Guilherme Hartmann Sales. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
