a55491ef852b0c8bccc36b8251f13187f14518ba
Guilherme Hartmann Sales <guilhermehsales@gmail.com>
2017-10-12 02:40:17 -0300

    Subs es 03

--- lista02/03.c
+++ lista02/03.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
-	float x, y, z, v;
-	scanf("%f %f %f", &x, &y, &z);
-	v = x * y * z;
-	printf("%.2f\n", v);
+	
+	long double a, b, t, p, a1, pi;
+	
+	a = 1;
+	b = 1 / (sqrt(2));
+	t = 0.25;
+	p = 1;
+	a1 = 0;
+
+	int i=0;
+
+	while(i < 9){
+		
+		a1 = (a + b) / 2;
+		b = sqrt(a * b);
+		t = t - (p * (pow((a - a1), 2)));
+		p = 2 * p;
+		a = a1;
+
+		i++;
+	}
+
+	pi = (pow((a + b), 2)) / (4 * t);
+
+	printf("\nO valor de PI eh : %.20Lf\n\n", pi);
+
 	return 0;
 }
